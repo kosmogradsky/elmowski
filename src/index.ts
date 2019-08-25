d7e5666d072a31693eb1bbdc4a14a25a73b2de20
Evgeny Kosmogradsky-Ivanov <kosmogradsky@gmail.com>
2019-08-25 10:56:18 +0200

    use redux instead of custom store implementation

--- src/index.ts
+++ src/index.ts
@@ -1,92 +1,99 @@
-import { Subject, Observable, merge } from 'rxjs';
-import { scan, startWith, map, filter, mergeMap } from 'rxjs/operators';
+import { Subject, Observable, merge } from "rxjs";
+import { filter } from "rxjs/operators";
+import {
+  Action,
+  Reducer,
+  StoreEnhancer,
+  createStore as createReduxStore,
+  Store
+} from "redux";
 
-export interface ValueConstructor {
+export interface Effect {
   readonly type: string;
 }
 
-export type Loop<S> = [S, ValueConstructor];
-export type Reducer<S, A> = (prevState: S, action: A) => S;
-export type LoopReducer<S, A> = (prevState: S, action: A) => Loop<S>;
-export type Epic<A> = (effect$: Observable<ValueConstructor>) => Observable<A>;
+const typeOfBatch = "Batch";
+const typeOfEmpty = "Empty";
 
-const batchType = 'Core/Batch'
-const noEffType = 'Core/NoEff'
+export class Batch<E extends Effect> implements Effect {
+  readonly type = typeOfBatch;
 
-export class Batch implements ValueConstructor {
-  readonly type = batchType
-
-  constructor(
-    readonly effects: ValueConstructor[]
-  ) {}
+  constructor(readonly effects: (E | Batch<E> | Empty)[]) {}
 }
 
-class NoEff implements ValueConstructor {
-  readonly type = noEffType
+class Empty implements Effect {
+  readonly type = typeOfEmpty;
 }
 
-export const NO_EFF = new NoEff();
+export const EMPTY = new Empty();
 
-const isBatch = (effect: ValueConstructor): effect is Batch => effect.type === batchType
-const isNoEff = (effect: ValueConstructor): effect is NoEff => effect.type === noEffType
+const isBatch = <E extends Effect>(effect: E | Batch<E>): effect is Batch<E> =>
+  effect.type === typeOfBatch;
+const isEmpty = <E extends Effect>(effect: E | Empty): effect is Empty =>
+  effect.type === typeOfEmpty;
 
-export const combineEpics = <A>(...epics: Epic<A>[]): Epic<A> => effect$ => merge(
-  ...epics.map(epic => epic(effect$))
-)
-
-export const ofType = <R extends ValueConstructor>(...keys: R['type'][]) =>
-  (source: Observable<ValueConstructor>): Observable<R> =>
-    source.pipe(filter((value): value is R => keys.includes(value.type)));
-
-export interface Store<S, A> {
-  state$: Observable<S>;
-  dispatch: (action: A) => void;
-  destroy: () => void;
-}
-
-export const createStore = <S, A>(
-  initialLoop: Loop<S>,
-  reducer: LoopReducer<S, A>,
-  epic: Epic<A>
-): Store<S, A> => {
-  const actionSubject = new Subject<A>();
-
-  const loop$ = actionSubject.pipe(
-    scan<A, Loop<S>>(
-      ([prevState], action) => reducer(prevState, action),
-      initialLoop,
-    ),
-    startWith(initialLoop),
-  );
-
-  const state$ = loop$.pipe(map(([state]) => state));
-
-  const effect$ = loop$.pipe(
-    map(([_, effect]) => effect),
-    mergeMap(effect => isBatch(effect) ? effect.effects : [effect]),
-    filter(effect => !isNoEff(effect))
-  );
-  const epicSubscription = epic(effect$).subscribe();
-
-  return {
-    state$,
-    dispatch: (action: A) => actionSubject.next(action),
-    destroy: () => {
-      actionSubject.complete();
-
-      // observables from epic() may derive not only from effect$
-      epicSubscription.unsubscribe();
-    }
+const toFlatArray = <E extends Effect>(effect: E | Empty | Batch<E>): E[] => {
+  if (isEmpty(effect)) {
+    return [];
   }
-}
 
-export interface CreateStore<S, A> {
-  (
-    initialLoop: Loop<S>,
-    reducer: LoopReducer<S, A>,
-    epic: Epic<A>
-  ): Store<S, A>
-}
+  if (isBatch(effect)) {
+    return effect.effects.reduce<E[]>((acc, effect) => {
+      return acc.concat(toFlatArray(effect));
+    }, []);
+  }
 
-export const createEnhancedStore = <S, A>(...enhancers: ((param: CreateStore<S, A>) => CreateStore<S, A>)[]) =>
-  enhancers.reduce((nextCreateStore, enhancer) => enhancer(nextCreateStore), createStore as CreateStore<S, A>)
+  return [effect];
+};
+
+export type Loop<S, E extends Effect> = [S, E | Batch<E> | Empty];
+export type LoopReducer<S, A, E extends Effect> = (
+  prevState: S,
+  action: A
+) => Loop<S, E>;
+export type Epic<A> = (effect$: Observable<Effect>) => Observable<A>;
+
+export const combineEpics = <A>(...epics: Epic<A>[]): Epic<A> => effect$ =>
+  merge(...epics.map(epic => epic(effect$)));
+
+export const ofType = <R extends Effect>(...keys: R["type"][]) => (
+  source: Observable<Effect>
+): Observable<R> =>
+  source.pipe(filter((value): value is R => keys.includes(value.type)));
+
+export const createStore = <
+  S,
+  A extends Action,
+  E extends Effect,
+  Ext,
+  StateExt
+>(
+  initialLoop: Loop<S, E>,
+  reducer: LoopReducer<S, A, E>,
+  epic: Epic<A>,
+  enhancer?: StoreEnhancer<Ext, StateExt>
+): Store<S & StateExt, A> & Ext => {
+  const effectSubject = new Subject<E>();
+
+  const liftReducer = (loopReducer: LoopReducer<S, A, E>): Reducer<S, A> => (
+    state,
+    action
+  ) => {
+    const [model, effect] =
+      state === undefined ? initialLoop : loopReducer(state, action);
+
+    toFlatArray(effect).forEach(eff => {
+      effectSubject.next(eff);
+    });
+
+    return model;
+  };
+
+  const store = createReduxStore(liftReducer(reducer), enhancer);
+
+  epic(effectSubject).subscribe(action => {
+    store.dispatch(action);
+  });
+
+  return store;
+};
