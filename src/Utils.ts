323fe8fecf324abd75fe76fa9eaae6301037be95
Evgeny Kosmogradsky-Ivanov <kosmogradsky@gmail.com>
2019-12-11 17:18:07 +0100

    fix groupByTracker type

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,4 +1,4 @@
-import { Observable, ObservedValueOf } from "rxjs";
+import { Observable } from "rxjs";
 import {
   groupBy,
   mergeMap,
@@ -7,16 +7,13 @@ import {
   filter
 } from "rxjs/operators";
 
-export const groupByTracker = <
-  T extends { tracker?: string },
-  R extends Observable<any>
->(
-  project: (value: T, index: number) => R,
+export const groupByTracker = <T extends { tracker?: string }, R>(
+  project: (value: T, index: number) => Observable<R>,
   cancelTracker$: Observable<string>
 ) => {
   const defaultTracker = Symbol("defaultTracker");
 
-  return (source$: Observable<T>): Observable<ObservedValueOf<R>> =>
+  return (source$: Observable<T>): Observable<R> =>
     source$.pipe(
       groupBy(effect => effect.tracker || defaultTracker),
       mergeMap(group => {
